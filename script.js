9cb3b5b9626b94ffc32ec70e2f4106306a7db3c1
joshuaFelicilda <joshuafelicilda248@gmail.com>
2026-05-31 22:06:15 +0800

    Add new function and variables
    Wrote the logic to get the human choice by creating getHumanChoice() using prompt() method.
    And declared two variables with initial values.

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function getComputerChoice() {
     const computerChoice = Math.random();
     return computerChoice;
 }
+
+// Human choice logic
+function getHumanChoice() {
+    const humanChoice = prompt("Choose: rock, paper, or scissors?");
+    return humanChoice;
+}
+
+// Declaring players score variables
+const computerScore = 0
+const humanScore = 0
