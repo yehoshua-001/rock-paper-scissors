598657a61f4df7477856e216f87b750a38fdf593
joshuaFelicilda <joshuafelicilda248@gmail.com>
2026-06-02 15:02:19 +0800

    Edit script.js and add new functions
    Modified getComputerChoice(), added and edited some variables,
    and added new functions such as the logic for single round and win/lose logic.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Computer choice logic
 function getComputerChoice() {
-    const computerChoice = Math.random();
-    return computerChoice;
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 // Human choice logic
@@ -10,6 +16,55 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-// Declaring players score variables
-const computerScore = 0
-const humanScore = 0
+// Variables
+let computerScore = 0
+let humanScore = 0
+let round = 1
+
+// Single round logic
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    console.log(`Round ${round}`)
+    console.log(`You chose ${humanChoice}! Computer chose ${computerChoice}!`)
+    // Comparing results
+    if (humanChoice === computerChoice) {
+        console.log(`It is draw! You both chose ${humanChoice}`)
+    } else {
+        if (humanChoice === "rock") {
+            if (computerChoice === "scissors") {
+                win();
+            } else {
+                lose();
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                win();
+            } else {
+                lose();
+            }
+        } else if (humanChoice === "scissors") {
+            if (computerChoice === "paper") {
+                win();
+            } else {
+                lose();
+            }
+        }
+    }
+
+    function win() {
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        console.log(`Your score: ${humanScore}`)
+        console.log(`Computer score: ${computerScore}`)
+    }
+
+    function lose() {
+        computerScore++;
+        console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`)
+        console.log(`Your score: ${humanScore}`)
+        console.log(`Computer score: ${computerScore}`)
+    }
+}
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
