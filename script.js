bf85dffc7969560a57b862a4f8a5538b672008c6
joshuaFelicilda <joshuafelicilda248@gmail.com>
2026-06-19 11:29:35 +0800

    Edit script.js
    Temporarily removed the for-loop for 5 rounds, added DOM elements,
    changed the getComputerChoice() and playRound(), added text display for
    results, and added event listener that calls back playRound() for
    buttons and each selection.

--- script.js
+++ script.js
@@ -1,83 +1,82 @@
-// Computer choice logic which randomly selects a number from 3 choices
 function letsPlay() {
     alert("Let's play Rock Paper Scissor!")
 }
 letsPlay();
 
-function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        return "rock";
-    } else if (computerChoice === 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-// Human choice logic which allows the user to input their choice
-function getHumanChoice() {
-    const humanChoice = prompt("Now choose! rock, paper, or scissors?");
-    return humanChoice;
-}
-
-// Initializing variables that will be used to play the game or the logic of the whole game
+// Global variables
 let computerScore = 0
 let humanScore = 0
 let round = 1
 
-function playGame() {
-    // Loop the game 5 times because it will play for 5 rounds
-    // And calling playRound() each round
-    for (i = 0; i < 5; i++) {
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);        
-    }
+// DOM elements
+const buttons = document.querySelectorAll('button');
+const choices = ['rock', 'paper', 'scissors'];
+const displayRound = document.querySelector('#round');
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const result = document.querySelector('#result');
+const score = document.querySelector('#score');
 
-    // Single round logic
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase()
-        console.log(`Round ${round}`)
-        console.log(`You chose ${humanChoice} | Computer chose ${computerChoice}`)
-        // Comparing results
-        if (humanChoice === computerChoice) {
-            console.log(`It is draw! You both chose ${humanChoice}.`)
-            console.log(`You ${humanScore} | ${computerScore} Computer`)
-        } else {
-            if (humanChoice === "rock") {
-                if (computerChoice === "scissors") {
-                    win();
-                } else {
-                    lose();
-                }
-            } else if (humanChoice === "paper") {
-                if (computerChoice === "rock") {
-                    win();
-                } else {
-                    lose();
-                }
-            } else if (humanChoice === "scissors") {
-                if (computerChoice === "paper") {
-                    win();
-                } else {
-                    lose();
-                }
+// Computer choice logic which randomly selects a number from 3 choices
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * choices.length);
+    return choices[randomNum];
+}
+
+// Single round logic
+function playRound(humanChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = getComputerChoice();
+    displayRound.textContent = `Round: ${round}`;
+    player.textContent = `You chose ${humanChoice.toUpperCase()}`;
+    computer.textContent = `Computer chose ${computerChoice.toUpperCase()}`;
+    // Comparing results
+    if (humanChoice === computerChoice) {
+        result.textContent = `It is draw!`;
+        score.textContent = `You ${humanScore} | ${computerScore} Computer`;
+    } else {
+        if (humanChoice === "rock") {
+            if (computerChoice === "scissors") {
+                win();
+            } else {
+                lose();
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                win();
+            } else {
+                lose();
+            }
+        } else if (humanChoice === "scissors") {
+            if (computerChoice === "paper") {
+                win();
+            } else {
+                lose();
             }
         }
-        // Function to call if win
-        function win() {
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-            console.log(`You ${humanScore} | ${computerScore} Computer`)
-        }
-        // Function to call if lose
-        function lose() {
-            computerScore++;
-            console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`)
-            console.log(`You ${humanScore} | ${computerScore} Computer`)
-        }
-        round++;
     }
+        
+    // Function to call if win
+    function win() {
+        humanScore++;
+        result.textContent = `You won! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
+        score.textContent = `You ${humanScore} | ${computerScore} Computer`;
+    }
+    // Function to call if lose
+    function lose() {
+        computerScore++;
+        result.textContent = `Computer won! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`;
+        score.textContent = `You ${humanScore} | ${computerScore} Computer`;
+    }
+    round++;
 }
-playGame();
+
+// Human choice logic for each button
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        const humanChoice = button.getAttribute('id');
+        playRound(humanChoice);
+    })
+});
+
+
