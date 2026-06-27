70197befbd92b5ed5fe01927b59d3544c7870aec
yehoshua-001 <joshuafelicilda248@gmail.com>
2026-06-27 19:46:24 +0800

    Edit script.js
    Added new feature that animates the hands each time the buttons are
    clicked, and refactored the code for readability.

--- script.js
+++ script.js
@@ -11,26 +11,7 @@ let humanScore = 0;
 const winningScore = 5;
 let round = 1;
 
-function getComputerChoice(){
-    const randomNum = Math.floor(Math.random() * choices.length);
-    return computerChoice = choices[randomNum];
-}
-
-function playRound(humanChoice){
-    computerChoice = getComputerChoice();
-    const computerChoiceImg = document.querySelector('#computerChoiceImg');
-    if(computerChoice === "rock"){
-        computerChoiceImg.setAttribute("src", "./images/computer/rock.png");
-    }
-    else if(computerChoice === "paper"){
-        computerChoiceImg.setAttribute("src", "./images/computer/paper.png");
-    }
-    else{
-        computerChoiceImg.setAttribute("src", "./images/computer/scissors.png");
-    }
-
-    displayRound.textContent = `Round ${round}`;
-
+function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
         result.textContent = `DRAW`;
     }
@@ -72,14 +53,9 @@ function playRound(humanChoice){
     }
     round++;
 
-    // Game winner logic
-    const removeBtn = document.querySelector('.choices')
-    const addBtn = document.querySelector('.addBtn');
-    if(humanScore === winningScore){
-        result.setAttribute('style', 'font-size: 4rem;');
-        result.textContent = `You win!`;
-        removeBtn.remove();
-
+    // Spawn create 'Play again?' button
+    function playAgainBtn(){
+        const addBtn = document.querySelector('.addBtn');
         const playAgainBtn = document.createElement('a');
         playAgainBtn.textContent = 'Play again?';
         playAgainBtn.setAttribute('id', 'playAgainBtn');
@@ -91,17 +67,19 @@ function playRound(humanChoice){
         playAgainBtn.addEventListener('mouseleave', () => {
             playAgainBtn.classList.remove('playAgainBtn-hovered');
         });
-        addBtn.appendChild(playAgainBtn);
-        
-        // Celebration effects using 'confetti.js.org' library
+        addBtn.appendChild(playAgainBtn);        
+    }
+
+    // Celebration effects using 'confetti.js.org' library
+    function celebration(){
         const duration = 15 * 1e3,
-            animationEnd = Date.now() + duration,
-            defaults = {
-                startVelocity: 30,
-                spread: 360,
-                ticks: 60,
-                zIndex: 0
-            };
+        animationEnd = Date.now() + duration,
+        defaults = {
+            startVelocity: 30,
+            spread: 360,
+            ticks: 60,
+            zIndex: 0
+        };
         function randomInRange(min, max){
             return Math.random() * (max - min) + min;
         }
@@ -124,77 +102,58 @@ function playRound(humanChoice){
                 }
             }));
         }, 250);
+    }
+
+    // Game winner logic
+    const removeBtn = document.querySelector('.choices')
+    if(humanScore === winningScore){
+        result.setAttribute('style', 'font-size: 4rem;');
+        result.textContent = 'You win!';
+        removeBtn.remove();
+        playAgainBtn();
+        celebration();
     }
     else if(computerScore === winningScore){
         result.setAttribute('style', 'font-size: 4rem;');
-        result.textContent = `Computer wins!`
+        result.textContent = 'Computer wins!';
         removeBtn.remove();
-
-        const playAgainBtn = document.createElement('a');
-        playAgainBtn.textContent = 'Play again?';
-        playAgainBtn.setAttribute('id', 'playAgainBtn');
-        playAgainBtn.setAttribute('href', 'game.html');
-        playAgainBtn.classList.add('playAgainBtn');
-        playAgainBtn.addEventListener('mouseenter', () => {
-            playAgainBtn.classList.add('playAgainBtn-hovered');
-        });
-        playAgainBtn.addEventListener('mouseleave', () => {
-            playAgainBtn.classList.remove('playAgainBtn-hovered');
-        });
-        addBtn.appendChild(playAgainBtn);
-
-        const duration = 15 * 1e3,
-            animationEnd = Date.now() + duration,
-            defaults = {
-                startVelocity: 30,
-                spread: 360,
-                ticks: 60,
-                zIndex: 0
-            };
-        function randomInRange(min, max){
-            return Math.random() * (max - min) + min;
-        }
-        const interval = setInterval(function(){
-            const timeLeft = animationEnd - Date.now();
-            if(timeLeft <= 0) return clearInterval(interval);
-            const particleCount = 50 * (timeLeft / duration);
-            confetti(Object.assign({}, defaults, {
-                particleCount,
-                origin: {
-                    x: randomInRange(.1, .3),
-                    y: Math.random() - .2
-            }
-        }));
-        confetti(Object.assign({}, defaults, {
-            particleCount,
-            origin: {
-                x: randomInRange(.7, .9),
-                y: Math.random() - .2
-                }
-            }));
-        }, 250);
+        playAgainBtn();
+        celebration();
     }
 }
 
-// Choices buttons that calls back playRound() function
+// Choices buttons for player to choose their checker, calls back playRound(),
+// generate computer choice randomly, and animate the hands for every click.
 const buttons = document.querySelectorAll('button');
-buttons.forEach(button =>{
+buttons.forEach(button => {
     button.addEventListener('click', function(){
         const humanChoice = button.getAttribute('id');
-        playRound(humanChoice);
-    });
-    button.addEventListener('click', function(){
-        const playerChoice = button.getAttribute('id');
-        const playerChoiceImg = document.querySelector('#playerChoiceImg');
-        if(playerChoice === "rock"){
-            playerChoiceImg.setAttribute("src", "./images/player/rock.png")
-        }
-        else if(playerChoice === "paper"){
-            playerChoiceImg.setAttribute("src", "./images/player/paper.png")
-        }
-        else{
-            playerChoiceImg.setAttribute("src", "./images/player/scissors.png")
-        }
+        // Get computer choice
+        const randomNum = Math.floor(Math.random() * choices.length);
+        const computerChoice = choices[randomNum];
+        const humanChoiceImg = document.querySelector('#humanChoiceImg');
+        const computerChoiceImg = document.querySelector('#computerChoiceImg');
+        displayRound.textContent = `Round ${round}`;
+        humanChoiceImg.setAttribute(`src`, `./images/player/rock.png`);
+        computerChoiceImg.setAttribute(`src`, `./images/computer/rock.png`);
+        result.textContent ="ROCK PAPER SCISSORS";
+        setTimeout(() => {
+            playRound(humanChoice, computerChoice);
+            humanChoiceImg.setAttribute(`src`, `./images/player/${humanChoice}.png`);
+            computerChoiceImg.setAttribute(`src`, `./images/computer/${computerChoice}.png`);
+        }, 2000);
+        humanChoiceImg.style.animation = 'shakeHuman 0.5s ease-in-out 3';
+        computerChoiceImg.style.animation = 'shakeComputer 0.5s ease-in-out 3';
     });
 })
 
+// To reset the animation for every click
+const hands = document.querySelectorAll('img');
+hands.forEach(hand => {
+    hand.addEventListener("animationend", function(){
+        const humanHand = document.querySelector('#humanChoiceImg');
+        const computerHand = document.querySelector('#computerChoiceImg');
+        humanHand.style.animation = '';
+        computerHand.style.animation = '';
+    });
+});
